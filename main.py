042997d4e527941f7596b5c56f7a7503779e8e4d
kishorewolfe <kishore.techie28@gmail.com>
2025-07-27 19:22:54 +0530

    PORT changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
 from services.ai_service import process_query
 from services.property_service import get_hotels_in_dubai, search_hotels
+import uvicorn
 
 app = FastAPI()
 
@@ -14,7 +15,8 @@ app.add_middleware(
     allow_headers=["*"]
 )
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))  # Default to 4000 if PORT not set
+    port = int(os.environ.get("PORT", 8000))  # Default to 4000 if PORT not set
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 @app.get("/")
 async def root():
